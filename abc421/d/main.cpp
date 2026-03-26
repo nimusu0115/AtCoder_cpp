84c3838c7291e82dce707aba2645583a145d2f14
nimusu0115 <kyortarokatakura@gmail.com>
2026-03-26 16:17:50 +0900

    E

--- abc421/d/main.cpp
+++ abc421/d/main.cpp
@@ -12,4 +12,102 @@ template<class T> void chmax(T& a,T b){ if(a < b) a = b;}
 template<class T> void chmin(T& a,T b){ if(a > b) a = b;}
 
 int main() {
+    ll Rt,Ct,Ra,Ca;
+    cin >> Rt >> Ct >> Ra >> Ca;
+    ll N,M,L;
+    cin >> N >> M >> L;
+    queue<pair<char,ll>> A,B;
+    rep(i,M){
+        char s;
+        ll a;
+        cin >> s >> a;
+        A.push({s,a});
+    }
+    rep(i,L){
+        char t;
+        ll b;
+        cin >> t >> b;
+        B.push({t,b});
+    }
+
+    if((Rt + Ct) % 2 != (Ra + Ca) % 2){
+        cout << 0 << endl;
+        return 0;
+    }
+    ll ans = 0;
+    while(!A.empty() || !B.empty()){
+        ll x = min(A.front().second,B.front().second);
+        if(Rt == Ra && Ct == Ca){
+            if(A.front().first == B.front().first){
+                ans += x;
+            }
+        }
+        else{
+            if(Rt == Ra){
+                if(Ct < Ca && A.front().first == 'R' && B.front().first == 'L'){
+                    if(Ca - Ct <= x * 2) ans++;
+                }
+                if(Ca < Ct && A.front().first == 'L' && B.front().first == 'R'){
+                    if(Ct - Ca <= x * 2) ans++;
+                }
+            }
+            if(Ct == Ca){
+                if(Rt < Ra && A.front().first == 'D' && B.front().first == 'U'){
+                    if(Ra - Rt <= x * 2) ans++;
+                }
+                if(Ra < Rt && A.front().first == 'U' && B.front().first == 'D'){
+                    if(Rt - Ra <= x * 2) ans++;
+                }
+            }
+            if(Ct - Ca == Rt - Ra){
+                if(Ra < Rt){
+                    if(A.front().first == 'U' && B.front().first == 'R'){
+                        if(Rt - Ra <= x) ans++;
+                    }
+                    if(A.front().first == 'L' && B.front().first == 'D'){
+                        if(Rt - Ra <= x) ans++;
+                    }
+                }
+                if(Rt < Ra){
+                    if(A.front().first == 'D' && B.front().first == 'L'){
+                        if(Ra - Rt <= x) ans++;
+                    }
+                    if(A.front().first == 'R' && B.front().first == 'U'){
+                        if(Ra - Rt <= x) ans++;
+                    }
+                }
+            }
+            if(Ca - Ct == Rt - Ra){
+                if(Ra < Rt){
+                    if(A.front().first == 'U' && B.front().first == 'L'){
+                        if(Rt - Ra <= x) ans++;
+                    }
+                    if(A.front().first == 'R' && B.front().first == 'D'){
+                        if(Rt - Ra <= x) ans++;
+                    }
+                }
+                if(Rt < Ra){
+                    if(A.front().first == 'L' && B.front().first == 'U'){
+                        if(Ra - Rt <= x) ans++;
+                    }
+                    if(A.front().first == 'D' && B.front().first == 'R'){
+                        if(Ra - Rt <= x) ans++;
+                    }
+                }
+            }
+        }
+        if(A.front().first == 'U') Rt -= x;
+        if(A.front().first == 'D') Rt += x;
+        if(A.front().first == 'L') Ct -= x;
+        if(A.front().first == 'R') Ct += x;
+        if(B.front().first == 'U') Ra -= x;
+        if(B.front().first == 'D') Ra += x;
+        if(B.front().first == 'L') Ca -= x;
+        if(B.front().first == 'R') Ca += x;
+        A.front().second -= x;
+        B.front().second -= x;
+        if(A.front().second == 0) A.pop();
+        if(B.front().second == 0) B.pop();
+    }
+    cout << ans << endl;
 }
--- abc450/e/main.cpp
+++ abc450/e/main.cpp
@@ -5,11 +5,121 @@ using namespace std;
 #define rrep(i,n) for(int i = n - 1;i >= 0;i--)
 #define onerep(i,n) for(int i = 1;i <= n;i++)
 #define ALL(a)  (a).begin(),(a).end()
-const int inf = 1<<30;
-const ll llinf = 1LL<<60;
+const int inf = 1'000'000'000;
+const ll llinf = 2'000'000'000'000'000'000LL;
 const double PI = 3.141592653589;
 template<class T> void chmax(T& a,T b){ if(a < b) a = b;}
 template<class T> void chmin(T& a,T b){ if(a > b) a = b;}
 
 int main() {
+    string X,Y;
+    cin >> X >> Y;
+    int q;
+    cin >> q;
+
+    map<char,ll> cX,cY;
+    for(auto c : X) cX[c]++;
+    for(auto c : Y) cY[c]++;
+
+    vector<ll> length;
+    ll a = X.size(),b = Y.size();
+    length.push_back(a);
+    length.push_back(b);
+    while(b <= llinf){
+        length.push_back(a + b);
+        b += a;
+        a = b - a;
+    }
+
+    while(q > 0){
+        q--;
+
+        ll L,R;
+        char C;
+        cin >> L >> R >> C;
+
+        vector<ll> bunkai(length.size(),0);
+
+        queue<tuple<int,ll,ll>> Q;
+        auto itr = lower_bound(ALL(length),R);
+        int start = distance(length.begin(),itr);
+
+        Q.push({start,L,R});
+
+        ll ans = 0;
+        while(!Q.empty()){
+            int n = get<0>(Q.front());
+            ll l = get<1>(Q.front());
+            ll r = get<2>(Q.front());
+            Q.pop();
+
+            if(n == 0){
+                for(int i = l;i <= r;i++){
+                    if(X[i] == C) ans++;
+                }
+            }
+            else if(n == 1){
+                for(int i = l;i <= r;i++){
+                    if(Y[i] == C) ans++;
+                }
+            }
+            else{
+                if(l == 1){
+                    if(r == length[n]){
+                        bunkai[n - 1]++;
+                        bunkai[n - 2]++;
+                    }
+                    if(length[n - 1] < r && r < length[n]){
+                        bunkai[n - 1]++;
+                        Q.push({n - 2,1,r - length[n - 1]});
+                    }
+                    if(r == length[n - 1]){
+                        bunkai[n - 1]++;
+                    }
+                    if(r < length[n - 1]){
+                        Q.push({n - 1,1,r});
+                    }
+                }
+                if(1 < l && l < length[n - 1] + 1){
+                    if(r == length[n]){
+                        Q.push({n - 1,l,length[n - 1]});
+                        bunkai[n - 2]++;
+                    }
+                    if(length[n - 1] < r && r < length[n]){
+                        Q.push({n - 1,l,length[n - 1]});
+                        Q.push({n - 2,1,r - length[n - 1]});
+                    }
+                    if(r == length[n - 1]){
+                        Q.push({n - 1,l,length[n - 1]});
+                    }
+                    if(r < length[n - 1]){
+                        Q.push({n - 1,l,r});
+                    }
+                }
+                if(l == length[n - 1] + 1){
+                    if(r == length[n]){
+                        bunkai[n - 2]++;
+                    }
+                    if(r < length[n]){
+                        Q.push({n - 2,1,r - length[n - 1]});
+                    }
+                }
+                if(length[n - 1] + 1 < l){
+                    Q.push({n - 2,l - length[n - 1],r - length[n - 1]});
+                }
+            }
+        }
+        rrep(i,length.size()){
+            if(i <= 1) continue;
+            bunkai[i - 1] += bunkai[i];
+            bunkai[i - 2] += bunkai[i];
+        }
+        ans += bunkai[0] * cX[C];
+        ans += bunkai[1] * cY[C];
+
+        cout << ans << endl;
+        //rep(i,start) cout << bunkai[i] << endl;
+        //cout << start << endl;
+    }
+    //for(auto v : length) cout << v << endl;
 }
