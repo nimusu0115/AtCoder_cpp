40ff5dab16879b848629bc2a8694ec0a5470e6d2
nimusu0115 <kyortarokatakura@gmail.com>
2026-08-02 14:35:25 +0900

    rrr

--- library/BFS_grid.cpp
+++ library/BFS_grid.cpp
@@ -9,46 +9,46 @@ vector<vector<bool>> seen;
 vector<vector<int>> dist;
 //到達可能性（グリッド）
 void bfs_seen(pair<int,int> start){
-  queue<pair<int,int>> Q;
-  Q.push(start);
-  dist[start.first][start.second] = 0;
-  while(!Q.empty()){
-    int h = Q.front().first;
-    int w = Q.front().second;
-    Q.pop();
-    rep(dir,4){
-      int nh = h + dx[dir];
-      int nw = w + dy[dir];
+	queue<pair<int,int>> Q;
+	Q.push(start);
+	dist[start.first][start.second] = 0;
+	while(!Q.empty()){
+		int h = Q.front().first;
+		int w = Q.front().second;
+		Q.pop();
+		rep(dir,4){
+			int nh = h + dx[dir];
+			int nw = w + dy[dir];
 
-      if(nh < 0 || H <= nh || nw < 0 || W <= nw) continue;
-      if(seen[nh][nw]) continue;
-      if(G[nh][nw] == '#') continue;
+			if(nh < 0 || H <= nh || nw < 0 || W <= nw) continue;
+			if(seen[nh][nw]) continue;
+			if(G[nh][nw] == '#') continue;
 
-      seen[nh][nw] = true;
-      Q.push({nh,nw});
-    }
-  }
+			seen[nh][nw] = true;
+			Q.push({nh,nw});
+		}
+	}
 }
 
 //最短距離（グリッド）
 void bfs_dist(pair<int,int> start){
-  queue<pair<int,int>> Q;
-  Q.push(start);
-  dist[start.first][start.second] = 0;
-  while(!Q.empty()){
-    int h = Q.front().first;
-    int w = Q.front().second;
-    Q.pop();
-    rep(dir,4){
-      int nh = h + dx[dir];
-      int nw = w + dy[dir];
+	queue<pair<int,int>> Q;
+	Q.push(start);
+	dist[start.first][start.second] = 0;
+	while(!Q.empty()){
+		int h = Q.front().first;
+		int w = Q.front().second;
+		Q.pop();
+		rep(dir,4){
+			int nh = h + dx[dir];
+			int nw = w + dy[dir];
 
-      if(dist[nh][nw] != -1) continue;
-      if(nh < 0 || H <= nh || nw < 0 || W <= nw) continue;
-      if(G[nh][nw] == '#') continue;
+			if(dist[nh][nw] != -1) continue;
+			if(nh < 0 || H <= nh || nw < 0 || W <= nw) continue;
+			if(G[nh][nw] == '#') continue;
 
-      dist[nh][nw] = dist[h][w] + 1;
-      Q.push({nh,nw});
-    }
-  }
+			dist[nh][nw] = dist[h][w] + 1;
+			Q.push({nh,nw});
+		}
+	}
 }
