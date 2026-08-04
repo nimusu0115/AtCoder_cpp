b04dced7fac6dd44b61a1773011273d4cd690fb0
nimusu0115 <kyortarokatakura@gmail.com>
2026-08-04 15:26:47 +0900

    343

---
+++ library/InsertionVolume.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define rep(i,n) for(int i = 0;i < n;i++)
+#define rrep(i,n) for(int i = n - 1;i >= 0;i--)
+#define onerep(i,n) for(int i = 1;i <= n;i++)
+#define ALL(a)  (a).begin(),(a).end()
+const int inf = 1<<30;
+const ll llinf = 1LL<<60;
+const double PI = 3.141592653589;
+template<class T> void chmax(T& a,T b){ if(a < b) a = b;}
+template<class T> void chmin(T& a,T b){ if(a > b) a = b;}
+
+ll getIntersectionVolume(ll min1[3], ll max1[3], ll min2[3], ll max2[3]) {
+    ll dx = max(0LL, min(max1[0], max2[0]) - max(min1[0], min2[0]));
+    ll dy = max(0LL, min(max1[1], max2[1]) - max(min1[1], min2[1]));
+    ll dz = max(0LL, min(max1[2], max2[2]) - max(min1[2], min2[2]));
+
+    return dx * dy * dz;
+}
+
+ll getIntersectionVolume3(ll min1[3], ll max1[3], ll min2[3], ll max2[3], ll min3[3], ll max3[3]) {
+    ll dx = max(0LL, min({max1[0], max2[0], max3[0]}) - max({min1[0], min2[0], min3[0]}));
+    ll dy = max(0LL, min({max1[1], max2[1], max3[1]}) - max({min1[1], min2[1], min3[1]}));
+    ll dz = max(0LL, min({max1[2], max2[2], max3[2]}) - max({min1[2], min2[2], min3[2]}));
+
+    return dx * dy * dz;
+}
+
+int main() {
+}
